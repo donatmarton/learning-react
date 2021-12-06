0b9376fb35500b7737138cabef82b2b39fb5ac87
Márton Donát Nagy <donat@donatmarton.com>
2021-12-06 11:28:06 +0100

    create and style main content

--- simple-info-site/src/index.js
+++ simple-info-site/src/index.js
@@ -6,17 +6,3 @@ import App from "./App"
 const root = document.getElementById('root');
 
 ReactDOM.render(<App />, root);
-
-// const contents = (
-//   <div>
-//     <img src={logo} width="40px" alt="" />
-//     <h1>Fun facts about React</h1>
-//     <ul>
-//       <li>Was first released in 2013</li>
-//       <li>Was originally created by Jordan Walke</li>
-//       <li>Has well over 100K stars on Github</li>
-//       <li>Is maintained by Facebook</li>
-//       <li>Powers thousands of enterprise apps, including mobile apps</li>
-//     </ul>
-//   </div>
-// );
