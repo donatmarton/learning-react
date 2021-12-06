957d2534fcec6d713d406e66fa08bac7f6ae1b99
Márton Donát Nagy <donat@donatmarton.com>
2021-12-06 15:24:47 +0100

    small additions

--- business-card/src/components/About.js
+++ business-card/src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <>
       <h2>About</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis donec et odio pellentesque diam. At in tellus integer feugiat scelerisque. Sed ullamcorper morbi tincidunt ornare massa eget. Gravida in fermentum et sollicitudin ac orci phasellus egestas. Lacus viverra vitae congue eu consequat ac.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </>
   )
 }
--- business-card/src/components/Interests.js
+++ business-card/src/components/Interests.js
@@ -4,7 +4,7 @@ export default function Interests() {
   return (
     <>
       <h2>Interests</h2>
-      <p>Blandit volutpat maecenas volutpat blandit aliquam etiam erat. Et malesuada fames ac turpis. Eu tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Sed nisi lacus sed viverra tellus in hac. Proin fermentum leo vel orci porta.</p>
+      <p>Blandit volutpat maecenas volutpat blandit aliquam etiam erat. Et malesuada fames ac turpis.</p>
     </>
   )
 }
