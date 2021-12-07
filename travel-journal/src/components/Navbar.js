2364d78b8825af6eb5a499ad27b0f7178273cdb1
Márton Donát Nagy <donat@donatmarton.com>
2021-12-07 19:52:26 +0100

    style the Navbar

--- travel-journal/src/components/Navbar.js
+++ travel-journal/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
+import globe from "../images/globe.png"
 
 export default function Navbar() {
   return (
     <nav>
-      
+      <img className="globe-image" src={globe} alt="" />
+      <h1 className="page-title">my travel journal.</h1>
     </nav>
   )
 }
