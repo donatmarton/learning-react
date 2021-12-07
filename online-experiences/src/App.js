8587b4389adfbd6b1bdc5cb3fa90fd77ddcdd3ec
Márton Donát Nagy <donat@donatmarton.com>
2021-12-07 18:22:06 +0100

    cards no longer hardcoded with data and are styled

--- online-experiences/src/App.js
+++ online-experiences/src/App.js
@@ -2,13 +2,27 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import data from "./data"
 
 function App() {
+  const cards = data.map( (cardData) => {
+    return (
+      <Card 
+        title={cardData.title}
+        coverImg={cardData.coverImg}
+        price={cardData.price}
+        stats={cardData.stats}
+        location={cardData.location}
+      />
+    )
+  });
   return (
     <div className="app">
       <Navbar />
       <Hero />
-      <Card />
+      <section className="cards-container">
+        {cards}
+      </section>
     </div>
   );
 }
--- online-experiences/src/components/Card.js
+++ online-experiences/src/components/Card.js
@@ -1,22 +1,20 @@
 import React from "react"
-import katie from "../images/katie-zaferes.png"
 import star from "../images/star.png"
 
-export default function Card()  {
+export default function Card(props)  {
+  const imagePath = `../images/${props.coverImg}`
   return (
-    <section className="cards-container">
-      <div className="card">
-        <p className="card-state">sold out</p>
-        <img className="card-image" src={katie} alt="" />
-        <div className="card-stats">
-          <img className="card-star" src={star} alt=""/>
-          <span>5.0 </span>
-          <span className="gray">(6) • </span>
-          <span className="gray">USA</span>
-        </div>
-        <p className="card-title">Life lessons with Katie Zaferes</p>
-        <p className="card-price"><span className="emphasised">From $136</span> / person</p>
+    <div className="card">
+      <p className="card-state">sold out</p>
+      <img className="card-image" src={imagePath} alt="" />
+      <div className="card-stats">
+        <img className="card-star" src={star} alt=""/>
+        <span>{props.stats.rating} </span>
+        <span className="gray">({props.stats.reviewCount}) • </span>
+        <span className="gray">{props.location}</span>
       </div>
-    </section>
+      <p className="card-title">{props.title}</p>
+      <p className="card-price"><span className="emphasised">From ${props.price}</span> / person</p>
+    </div>
   )
 }
