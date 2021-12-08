4164d4a4464867a929ca8747f98fc3648d316712
Márton Donát Nagy <donat@donatmarton.com>
2021-12-08 14:35:34 +0100

    can get a new meme from a database

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,18 +1,23 @@
 import React from "react";
-import meme from "../images/meme.png"
+import defaultMeme from "../images/meme.png"
+import memesData from "../memesData"
 
 export default function Meme() {
+  const [meme, setMeme] = React.useState(defaultMeme);
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomIndex = Math.floor(Math.random() * memesArray.length);
+    setMeme(memesArray[randomIndex].url);
+}
   return (
     <main>
-      <form>
-        <div class='form-row'>
-          <input id="first-input" name="first-input" type="text" placeholder="Shut up"/>
-          <input id="second-input" name="second-input" type="text" placeholder="and take my money"/>
-        </div>
-        <div class='form-row'>
-          <button id="get-meme" name="get-meme">Get a new meme image 🖼</button>
-        </div>
-      </form>
+      <div class='form-row'>
+        <input id="first-input" name="first-input" type="text" placeholder="Shut up"/>
+        <input id="second-input" name="second-input" type="text" placeholder="and take my money"/>
+      </div>
+      <div class='form-row'>
+        <button onClick={getMemeImage} id="get-meme" name="get-meme">Get a new meme image 🖼</button>
+      </div>
       <div className="meme">
         <img 
           className="meme-image"
