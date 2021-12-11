7f1b01e11859808765ec99d024296d1eaa9dcf16
Márton Donát Nagy <donat@donatmarton.com>
2021-12-11 19:07:24 +0100

    set up & style die component

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,11 +1,22 @@
 import React from "react"
-
+import Die from "./components/Die";
 
 function App() {
   return (
-    <div className="app">
-      
-    </div>
+    <main className="app">
+      <div className="dice">
+        <Die value={1}/>
+        <Die value={5}/>
+        <Die value={3}/>
+        <Die value={6}/>
+        <Die value={3}/>
+        <Die value={1}/>
+        <Die value={4}/>
+        <Die value={1}/>
+        <Die value={1}/>
+        <Die value={2}/>
+      </div>
+    </main>
   );
 }
 
