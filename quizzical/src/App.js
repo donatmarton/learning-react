1a6d6812c533e85d4a22e5a25d6692a91cbc281e
Márton Donát Nagy <donat@donatmarton.com>
2021-12-11 22:01:34 +0100

    title and quiz screens conditionally rendered

--- quizzical/src/App.js
+++ quizzical/src/App.js
@@ -1,9 +1,21 @@
+import React from "react"
+import TitleScreen from "./components/TitleScreen";
+import QuizScreen from "./components/QuizScreen";
 
 
 function App() {
+  const [showTitle, setShowTitle] = React.useState(true)
+
+  function hideTitle() {
+    setShowTitle(false);
+  }
+
   return (
     <div className="app">
-      
+      {showTitle ? 
+        <TitleScreen hideTitle={hideTitle}/> 
+        : 
+        <QuizScreen />}
     </div>
   );
 }
--- quizzical/src/components/TitleScreen.js
+++ quizzical/src/components/TitleScreen.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-export default function TitleScreen() {
+export default function TitleScreen(props) {
   return (
     <div className="title-screen--container">
       <h1 className="title-screen--title">Quizzical</h1>
       <p className="title-screen--description">Some description if needed</p>
-      <button className="title-screen--start-button" >
-        Start quit
+      <button 
+        className="title-screen--start-button" 
+        onClick={props.hideTitle}
+      >
+        Start quiz
       </button>
     </div>
   )
