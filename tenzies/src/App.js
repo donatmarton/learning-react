9fb2a04e9e054c4fea16592834a0b58673ee6671
Márton Donát Nagy <donat@donatmarton.com>
2021-12-11 19:31:20 +0100

    dice value saved in state, can be rolled

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -2,20 +2,37 @@ import React from "react"
 import Die from "./components/Die";
 
 function App() {
+  const [dieValues, setDieValues] = React.useState(generateRandomDice());
+  
+  function generateRandomDice() {
+    let dieValues = [];
+    for (let i = 0; i < 10; i++) {
+      dieValues.push( Math.floor(Math.random() * 6 + 1) );
+    }
+    return dieValues
+  }
+  const dieComponents = dieValues.map((dieValue, index) => (
+    <Die 
+      value={dieValue}
+      key={index}
+    />
+  ));
+
+  function rollDice() {
+    setDieValues(generateRandomDice());
+  }
+
   return (
     <main className="app">
       <div className="dice">
-        <Die value={1}/>
-        <Die value={5}/>
-        <Die value={3}/>
-        <Die value={6}/>
-        <Die value={3}/>
-        <Die value={1}/>
-        <Die value={4}/>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={2}/>
+        {dieComponents}
       </div>
+      <button 
+        className="roll-button"
+        onClick={rollDice}
+      >
+        Roll
+      </button>
     </main>
   );
 }
