a7fccf46e8cf2fb4cd16da0dfc617477f38b6747
Márton Donát Nagy <donat@donatmarton.com>
2021-12-12 20:14:36 +0100

    replace placeholder text

--- quizzical/src/components/TitleScreen.js
+++ quizzical/src/components/TitleScreen.js
@@ -4,7 +4,7 @@ export default function TitleScreen(props) {
   return (
     <div className="screen title-screen--container">
       <h1 className="title-screen--title">Quizzical</h1>
-      <p className="title-screen--description">Some description if neededSome description if neededSome description if neededSome description if neededSome description if neededSome description if neededSome description if neededSome description if needed</p>
+      <p className="title-screen--description">A fun quiz game.</p>
       <button 
         className="title-screen--start-button" 
         onClick={props.hideTitle}
