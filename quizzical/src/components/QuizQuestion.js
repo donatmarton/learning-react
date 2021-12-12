8c04850721c8e238c300740e85f1c30b72233110
Márton Donát Nagy <donat@donatmarton.com>
2021-12-12 14:22:48 +0100

    questions are radio buttons and are styled

--- quizzical/src/components/QuizQuestion.js
+++ quizzical/src/components/QuizQuestion.js
@@ -1,16 +1,53 @@
 import React from "react";
 
-export default function QuizQuestion() {
+export default function QuizQuestion(props) {
   return (
     <div className="quiz--question">
-      <h2 className="quiz--question-text">How would one say goodbye in spanish?How would one say goodbye in spanish?</h2>
-      <div className="quiz--answer-field">
-        <button className="quiz--answer">Adiós</button>
-        <button className="quiz--answer">Hola</button>
-        <button className="quiz--answer">Au revoir</button>
-        <button className="quiz--answer">Salir</button>
-      </div>
+      <fieldset className="quiz--question">
+        <legend className="quiz--question-text">How would one say goodbye in spanish?How would one say goodbye in spanish?</legend>
+        <div className="quiz--answer-radios">
+          <input 
+            type="radio"
+            id={"question"+props.id+"answer1"}
+            name={"question"+props.id}
+            value="unemployed"
+            // checked={}
+            // onChange={}
+          />
+          <label htmlFor={"question"+props.id+"answer1"}>Adiós</label>
+
+          <input 
+            type="radio"
+            id={"question"+props.id+"answer2"}
+            name={"question"+props.id}
+            value="unemployed"
+            // checked={}
+            // onChange={}
+          />
+          <label htmlFor={"question"+props.id+"answer2"}>Hola</label>
+
+          <input 
+            type="radio"
+            id={"question"+props.id+"answer3"}
+            name={"question"+props.id}
+            value="unemployed"
+            // checked={}
+            // onChange={}
+          />
+          <label htmlFor={"question"+props.id+"answer3"}>Au revoir</label>
+
+          <input 
+            type="radio"
+            id={"question"+props.id+"answer4"}
+            name={"question"+props.id}
+            value="unemployed"
+            // checked={}
+            // onChange={}
+          />
+          <label htmlFor={"question"+props.id+"answer4"}>Salir</label>
+        </div>
+      </fieldset> 
       <hr/>
-    </div>
+    </div> 
   )
 }
--- quizzical/src/components/QuizScreen.js
+++ quizzical/src/components/QuizScreen.js
@@ -4,9 +4,9 @@ import QuizQuestion from "./QuizQuestion";
 export default function QuizScreen() {
   return (
     <div className="screen quiz--container">
-      <QuizQuestion />
-      <QuizQuestion />
-      <QuizQuestion />
+      <QuizQuestion id={1} />
+      <QuizQuestion id={2} />
+      <QuizQuestion id={3} />
 
       <div className="quiz--controls">
         <p className="quiz--result">You scored 3/5 correct answers</p>
